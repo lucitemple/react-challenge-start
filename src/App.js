a0abb9a025810c6c95e44aa211b1bbefb5de91fb
Luci Temple <lucitemple@yahoo.com.au>
2021-04-29 11:55:02 +1000

    Add addContacts and addAppointment methods

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
 
 function App() {
   /*
   Define state variables for 
   contacts and appointments 
   */
+ const [contacts, setContacts] = useState([
+   {
+     contactName: "Jane Doe",
+     phone: "0414567855",
+     email: "yahoo@gmail.com",
+   },
+   {
+     contactName: "John Doe",
+     phone: "0414567855",
+     email: "yahoo@gmail.com",
+   },
+ ]);
+ const [appointments, setAppointments] = useState([
+   {
+     title: "Dentist",
+     contact: "Jane Doe",
+     date : "25-09-2021",
+     time: "10:30pm",
+   },
+ ]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
+  const addContacts = (contactName, phone, email) => {
+    let newContact = {
+      contactName: contactName,
+      phone, 
+      email,
+    };
+    setContacts((prev) => [newContact, ...prev]);
+  };
+
+    const addAppointments = (title, contact, date, time) => {
+      let newAppointment = {
+        title,
+        contact,
+        date,
+        time,
+      };
+      setAppointments((prev) => [newAppointment, ...prev]);
+    };
+
+  //console.log(addContacts(contacts));
   /*
   Implement functions to add data to
   contacts and appointments
@@ -38,6 +79,7 @@ function App() {
           <Route path={ROUTES.CONTACTS}>
              {/* Add props to ContactsPage */}
             <ContactsPage />
+            <ul>{contacts.map(contact => <li key={contact.phone}>{contact.name}</li>)}</ul>  
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
