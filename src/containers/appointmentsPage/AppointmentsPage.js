08336965aa5c5137a0e68e19663192cf2d472e3b
Luci Temple <lucitemple@yahoo.com.au>
2021-04-30 10:41:08 +1000

    Render TileList component and pass appointments props

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
 export const AppointmentsPage = (appointments, contacts, addAppointment) => {
   /*
@@ -44,6 +45,7 @@ export const AppointmentsPage = (appointments, contacts, addAppointment) => {
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList appointments={appointments} />
       </section>
     </div>
   );
