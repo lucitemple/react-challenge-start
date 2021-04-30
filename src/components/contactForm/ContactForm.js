b6cb7fcffd07d7db7702bc9ed1a519a910897417
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-04-30 12:01:19 +1000

    create input in contact form and update and add it to contactpage

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,19 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      {/* name input */}
+    <input type="text" placeholder="name" value={name}
+        onChange={({target}) => {setName(target.value)}}
+        required />
+      {/* phone numer input */}
+      <input type="number" placeholder='phone number'
+        pattern="(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)"   value={phone} onChange={({ target }) => { setPhone(target.value) }} required/>
+      {/* ^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$ */}
+      {/* email input  */}
+      <input type="text" placeholder='email' pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+        value={email} onChange={({ target }) => { setEmail(target.value) }} required />
+       <button>Submit</button>
+    </form>
   );
 };
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,17 +1,25 @@
 import React, {useState} from "react";
 
 export const ContactsPage = () => {
+
   /*
   Define state variables for 
   contact info and duplicate check
   */
- const [contact, setContact] = useState(
-   {
-     contactName: "Jane Doe",
-     phone: "0414567855",
-     email: "yahoo@gmail.com",
-     isDuplicate: false,
-   },);
+  const contacts = props.contacts;
+  const addContact = props.addContact;
+//  const [contact, setContact] = useState(
+//    {
+//      contactName: "Jane Doe",
+//      phone: "0414567855",
+//      email: "yahoo@gmail.com",
+//      isDuplicate: false,
+//    },);
+// local variables including boolean for duplication check
+const [name, setName] = useState('');
+const [phone, setPhone] = useState('');
+const [email, setEmail] = useState('');
+const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,21 +27,63 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!duplicate) {
+
+      addContact(name,phone, email);
+      // reseting values clearing the form
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect( () => {
+    for (const contact of contacts) {
+      if (name === contact.name) {
+        setDuplicate(true);
+      }
 
+      return;
+    }
+  });
+//   const array1 = ['a', 'b', 'c'];
+// const lett = array1[1]
+// console.log(lett)
+// let duplicate = false;
+
+// for (const element of array1) {
+//   if(element === lett){
+//     duplicate = true
+//   }else{
+//      duplicate = false;
+//   }
+//   console.log(element);
+//   console.log(duplicate);
+// }
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm 
+         name={name}
+          phone={phone}
+          email={email}
+          setName={setName}
+          setPhone={setPhone}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList
+          contactArray={props.contacts}
+        />
+      {/* <TileList {contact = props.contacts}/> */}
       </section>
     </div>
   );
